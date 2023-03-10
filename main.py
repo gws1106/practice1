11b2e3af20252c30a1f77e8025ec1e95ca64a3a6
gw1106 <gw1106@naver.com>
2023-03-10 13:39:03 +0900

    Master2 곱하기 함수 추가

---
+++ main.py
@@ -0,0 +1,15 @@
+from remainder import reaminder_func   #from 파이선파일이름 import 함수이름
+from square import square_func
+
+##함수
+def mul_func(n1, n2):
+    return n1 * n2
+
+
+##전역
+num1, num2, res = 100, 200, 0
+
+
+##메인 코드실행부
+res = mul_func(num1, num2)
+print(f"곱하기 결과 : {res}")
